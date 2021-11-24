c22f18a9b6e7f328ae60b6a2d7e26d75c27f6b0f
Kriz Royce A. Tahimic <62783701+KrizTahimic@users.noreply.github.com>
2021-11-24 22:39:25 +0800

    Update IterativeDeepeningSearch.java

--- Model/IterativeDeepeningSearch.java
+++ Model/IterativeDeepeningSearch.java
@@ -1,54 +1,132 @@
 package Model;
 
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.awt.Point;
+
+
 
 public class IterativeDeepeningSearch {
+    private Node ndRoot;
+    private int nIncrement;
     private Queue<Node> qFringe;
-    private Node ndCurrent;
-    private final static int nStartDepth = 3;
-    private final static int nDepthIncrement = 5;
-    private boolean bGoalFound = false;
-
-    public IterativeDeepeningSearch()
-    {
-        qFringe = new LinkedList<>();
-    }
     
-    private void ids(Node nStart)
+    /**
+     * Constructs the Iterative Deepening Search
+     * @param boardRoot intial state of the game
+     * @param pMinerCurrCoordinates coordanate where the miner is located at the start of the game
+     * @param nIncrement count of increment of depth limit
+     */
+    public IterativeDeepeningSearch (Cell[][] boardRoot, Point pMinerCurrCoordinates,int nIncrement)
     {
-        ndCurrent = nStart;
-        qFringe.add(nStart);
-        // Loop while not in Goal State
-        while (!bGoalFound) 
-            // Loop while the fringe is not yet empth 
-            // AND the Goal is NOT yet FOUND
-            while (     !qFringe.isEmpty() 
-                    ||  !bGoalFound)
-            {
-            //    if (isGoalState(curr.))
-           //     {
+        ndRoot = new Node('E', boardRoot, pMinerCurrCoordinates, null);
+        this.nIncrement = nIncrement;
+        qFringe = new PriorityQueue<Node>();
 
-              //  }
-
-            }
     }
-
 
     /**
-     * Checks if the current cell of the miner is the GOLD tile
-     * @param cMinerCurrCell Cell : current cell of the miner
-     * @return TRUE if the miner current cell is the Gold tile, otherwise FALSE;
+     * Searches and constructs the tree to find the goal state (Miner in Gold tile)
+     * @return the train of action to be done in the game to reach the goal state from left-to-right (first_action-to-last_action)
      */
-    private boolean isGoalState(Cell cMinerCurrCell)
+    public String ids ()
     {
-        boolean bGoalState = false;
+        boolean bFoundGoal = false; // Goal state not yet found
+        int nCurrDepthLimit = 2; 
+        int nCurrDepth = 1;
+        Node ndGoal = null;
 
-        if (cMinerCurrCell.isGold())
+        // MAIN IDS LOOP
+        do  // Loop until goal state is found
         {
-            bGoalState = true;
-        }
+            qFringe.clear(); // RESETS the fringe
+            qFringe.add(ndRoot); // Add the Root Node
 
-        return bGoalState;
+            // Depth Limited Search
+            do
+            {
+                // Check if Goal State
+                if (isGoalState(qFringe.peek().getState()[qFringe.peek().getMinerCoordinates().x][qFringe.peek().getMinerCoordinates().y]))
+                {
+                    bFoundGoal = true;
+                    ndGoal = qFringe.remove(); // Remove the head of the queue
+                    qFringe.clear(); // Clear fringe to exit loop and free memory
+                }
+                // Check if NOT yet reach the DEPTH LIMIT
+                else if(nCurrDepth < nCurrDepthLimit)
+                {
+                    // Produce children nodes
+                    produceChildren();
+
+                    // Add produced child to Fringe
+                    int nChildrentCount = qFringe.peek().getChildrenCount(); // Remove the head of the queue
+                    Node tempParent = qFringe.remove();
+                    for(int i=0;i < nChildrentCount; i++)
+                    {
+                        qFringe.add(tempParent.getChild(i));
+                    }
+
+                    nCurrDepth++;
+
+                }
+                
+
+            }while(!qFringe.isEmpty()); // Loop until qFringe is empty
+            
+
+            nCurrDepthLimit = nCurrDepthLimit + nIncrement; // Increment the max Depth Limit
+        }while (!bFoundGoal);
+
+        // Backtrack
+        return backtrack(ndGoal);
     }
+
+    /**
+     * Records the searched train of action to reach the goal state
+     * @param ndGoalNode The final state/action done to reach the goal state.
+     *                   Start of backtracking.
+     * @return the train of action to be done in the game to reach the goal state from left-to-right (first_action-to-last_action)
+     */
+    private String backtrack (Node ndGoalNode)
+    {
+        String strActions = "";
+
+        Node ndChild;
+        Node ndParent = ndGoalNode;
+        do
+        {
+            ndChild = ndParent;
+            strActions = ndChild.getAction() + strActions;
+            ndParent = ndChild.getParent();
+        }while(ndParent != null);
+
+        return strActions;
+    }
+
+    /**
+     * Constructs the children of the head of the qFringe
+     */
+    private void produceChildren ()
+    {
+        // Add Move Child
+        qFringe.peek().setChildState('M');
+
+        // Add Rotate Child
+        qFringe.peek().setChildState('S');
+
+        // Add Scan Child
+        qFringe.peek().setChildState('R');
+    }
+
+    /**
+     * Checks if the miner is currently in the Goal tile.
+     * Checks if the goal state is sufficed
+     * @param cMinerCell the Cell where miner is currently located
+     * @return True if miner is in Gold tile, otherwise false
+     */
+    private boolean isGoalState(Cell cMinerCell)
+    {
+        return cMinerCell.isMiner() && cMinerCell.isGold();
+    }
+
 }
